c8f997f28b901f58e8b91f0cc8c2eb412c152253
wouter martens <wouter.martens@gmail.com>
2011-05-07 17:03:49 +0200

    implemented a texture packer script and sprite frame cache

---
+++ thegrid/Scripts/PackTextures.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+# PackTextures.sh
+# bumba
+#RGBA4444
+# Created by Alain Hufkens on 14/02/11.
+# Copyright 2011 Nascom. All rights reserved.
+
+#!/bin/sh
+
+TP="/usr/local/bin/TexturePacker"
+
+if [ "${ACTION}" = "clean" ]
+then
+    echo "cleaning..."
+
+    rm thegrid/Resources/SpriteSheets/tile-assets.*
+
+else
+    echo "building..."
+
+# Game Art
+
+    ${TP} --smart-update \
+          --format cocos2d \
+          --data thegrid/Resources/SpriteSheets/tile-assets.plist \
+          --sheet thegrid/Resources/SpriteSheets/tile-assets.pvr.ccz \
+          --enable-rotation \
+          --dither-fs \
+          --shape-padding 1 \
+          --opt RGBA4444 \
+          --trim \
+          thegrid/Art/TileAssets/*.png
+
+fi
+exit 0
+
