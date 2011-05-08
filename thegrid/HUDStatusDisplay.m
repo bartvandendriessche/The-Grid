5f8101b1bd732934dbc899c423b8c687a578fb45
wouter martens <wouter.martens@gmail.com>
2011-05-08 13:41:41 +0200

    implemented the freebooter bitmap font

--- thegrid/HUDStatusDisplay.m
+++ thegrid/HUDStatusDisplay.m
@@ -12,4 +12,16 @@
 
 @synthesize label = _label;
 
+- (id)initWithFile:(NSString *)filename {
+    if ((self = [super initWithFile:filename])) {
+        self.label = [CCLabelBMFont labelWithString:@"0" fntFile:@"freebooter-hd.fnt"];
+        _label.scale = 0.7f;
+        _label.color = ccc3(200, 188, 150);
+        _label.position = ccp(self.contentSize.width / 2 + 30, self.contentSize.height / 2 - 10);
+        [self addChild:_label];
+    }
+    
+    return self;
+}
+
 @end
